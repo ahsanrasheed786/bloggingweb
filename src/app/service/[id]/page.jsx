aeeee4fa47e0469ada487aca0e55ef536b1f35bb
ahsanrasheed786 <rasheedahsan786@gmail.com>
2024-09-15 17:13:35 +0500

    solving error

--- src/app/service/[id]/page.jsx
+++ src/app/service/[id]/page.jsx
@@ -77,7 +77,7 @@ const ServicePage = ({ service }) => {
         <section className={styles.experienceSection}>
           <h2 className={styles.sectionTitle}> 
             <svg xmlns="http://www.w3.org/2000/svg" className={styles.svg}
-              viewBox="0 0 24 24"width="24"height="24"fill="none"stroke="currentColor"strokeWidth="2"strokeLinecap="round"strokeLinejoin="round"className={styles.experienceIcon}>
+              viewBox="0 0 24 24"width="24"height="24"fill="none"stroke="currentColor"strokeWidth="2"strokeLinecap="round"strokeLinejoin="round">
                <rect x="2" y="7" width="20" height="14" rx="2" ry="2"></rect>
               <path d="M16 3h-8v4h8V3z"></path>
               </svg>
@@ -91,7 +91,7 @@ const ServicePage = ({ service }) => {
         <section className={styles.educationSection}>
           <h2 className={styles.sectionTitle}>
           <svg className={styles.svg} xmlns="http://www.w3.org/2000/svg"viewBox="0 0 24 24" width="24" height="24"
-          fill="none"stroke="currentColor"strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"className={styles.educationIcon}>
+          fill="none"stroke="currentColor"strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
           <path d="M12 3L1 9l11 6 9-5.14V16a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3v-2"></path>
           <polyline points="22 9 12 15 2 9"></polyline></svg>
             Education
